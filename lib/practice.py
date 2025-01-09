b9800708dbf6f942a569523b4b6074bec84c2cd9
Garrett <hawkpdx@icloud.com>
2025-01-08 23:23:40 -0800

    completed assignment

---
+++ lib/practice.py
@@ -0,0 +1,10 @@
+hat_height_cm = 25
+my_height_cm = 210
+total_height_meters = hat_height_cm + my_height_cm / 100
+print(f"Height in meters = {total_height_meters}")
+      #to print a str in reverse, Python:
+# print(msg1[::-1].title())
+
+name = input('What is your name??: ')
+print("User Input")
+
